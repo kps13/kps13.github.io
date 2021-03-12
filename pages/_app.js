f4f9e659f47b845fca4fe03b9a2f6e865187b2b2
samda <muy.samda13@gmail.com>
2021-03-12 16:34:07 +0700

    add responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      once: false,
+      mirror: false,
+    });
+    window.addEventListener('touchmove', () => {
+      AOS.refresh()
+    }, false)
+  }, []);
+
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
 import Head from 'next/head'
 import ReactFullpage from '@fullpage/react-fullpage';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react'
 
 export default function Home() {
-  useEffect( () => {
-    AOS.init()
-  }, [])
   return (
     <ReactFullpage
       //fullpage options
@@ -20,26 +14,26 @@ export default function Home() {
             <div className="section">
               <div style={{ background: `url(./images/brands.png) `}} className="h-full w-full">
                 <div className="w-full h-full bg-white bg-opacity-70 flex flex-col justify-end items-center">
-                  <div className="self-start mb-20 py-6 bg-yellow-400 bg-opacity-80 w-full flex justify-center items-start">
-                    <h1 className="text-6xl text-bold text-gray-900">
+                  <div className="h-full self-start mb-10 md:mb-20 py-3 md:py-6 bg-yellow-400 bg-opacity-80 w-full flex justify-center items-center">
+                    <h1 className="text-2xl md:text-6xl text-bold text-gray-900">
                       ស្វាគមន៍មកកាន់ KPS Collection
                     </h1>
                   </div>
-                  <div className="w-5/6 rounded-md mb-10 px-6 py-6 bg-gray-900 bg-opacity-80 flex items-center justify-center">
-                    <h1 className="text-3xl text-bold text-yellow-300">
+                  <div className="w-5/6 rounded-md mb-4 md:mb-10 px-3 md:px-6 py-6 bg-gray-900 bg-opacity-80 flex items-center justify-center">
+                    <h1 className="text-lg md:text-3xl text-bold text-yellow-300">
                       ពួកយើងផ្តល់ជូននូវ សំលៀកបំពាក់ស្អាតៗ ទាន់សម័យ សំរាប់ បុរស នារី នឹង កុមារ
                     </h1>
                   </div>
-                  <div className="w-5/6 flex justify-between items-center bg-opacity-80 py-10 space-x-10">
-                    <img data-aos="fade-right" data-aos-duration="2000" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/baby.svg" />
-                    <img data-aos="fade-right" data-aos-duration="1500" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/girl.svg" />
-                    <img data-aos="fade-up" data-aos-duration="1000" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/boy.svg" />
-                    <img data-aos="fade-left" data-aos-duration="1500" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/women.svg" />
-                    <img data-aos="fade-left" data-aos-duration="2000" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/men.svg" />
+                  <div className="w-full md:w-5/6 grid grid-cols-2 justify-items-center gap-2 place-content-start md:flex md:justify-between md:items-center bg-opacity-80 py-4 md:py-10 md:space-x-10">
+                    <img data-aos="fade-right" data-aos-duration="2000" className="col-span-2 place-self-center h-20 md:h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/baby.svg" />
+                    <img data-aos="fade-right" data-aos-duration="1500" className="h-20 md:h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/girl.svg" />
+                    <img data-aos="fade-up" data-aos-duration="1000" className="h-20 md:h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/boy.svg" />
+                    <img data-aos="fade-left" data-aos-duration="1500" className="h-20 md:h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/women.svg" />
+                    <img data-aos="fade-left" data-aos-duration="2000" className="h-20 md:h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/men.svg" />
                   </div>
                   <div className="py-6 bg-yellow-400 bg-opacity-80 w-full flex justify-center items-start">
-                    <h1 className="text-6xl text-bold text-gray-900">
-                      នឹងមកដើលឆាប់ៗនេះ!
+                    <h1 className="text-2xl md:text-6xl text-bold text-gray-900">
+                      នឹងមកដល់ក្នុងពេលឆាប់ៗនេះ!
                     </h1>
                   </div>
                 </div>
@@ -52,17 +46,37 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="section" style={{ backgroundSize: 'cover', background: "url(./images/women.png) no-repeat center center" }}>
-              <div className="mx-auto w-full h-screen bg-white bg-opacity-30 flex justify-center items-center">
-                <div className="p-4 rounded-lg bg-gray-900 bg-opacity-60">
-                  <h1 className="text-6xl font-bold text-yellow-400">នារី</h1>
+            <div className="section">
+              <div className="w-full h-full flex justify-center items-center">
+                <img className="object-cover w-full h-screen absolute z-0" src="./images/women.png" />
+                <div className="absolute mx-auto w-full h-screen bg-white bg-opacity-30 flex justify-center items-center">
+                  <div className="p-4 rounded-lg bg-gray-900 bg-opacity-60">
+                    <h1 className="text-6xl font-bold text-yellow-400">នារី</h1>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="section" style={{ backgroundSize: 'cover', background: "url(./images/men.jpg) no-repeat center center" }}>
-              <div className="mx-auto w-full h-screen bg-white bg-opacity-30 flex justify-center items-center">
-                <div className="p-4 rounded-lg bg-gray-900 bg-opacity-60">
-                  <h1 className="text-6xl font-bold text-yellow-400">បុរស</h1>
+            <div className="section">
+              <div className="w-full h-full flex justify-center items-center">
+                <img className="object-cover w-full h-screen absolute z-0" src="./images/men.jpg" />
+                <div className="absolute mx-auto w-full h-screen bg-white bg-opacity-30 flex justify-center items-center">
+                  <div className="p-4 rounded-lg bg-gray-900 bg-opacity-70">
+                    <h1 className="text-6xl font-bold text-yellow-400">បរុស</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="section">
+              <div className="w-full h-full flex justify-center items-center">
+                <img className="object-cover w-full h-screen absolute z-0" src="./images/design.jpeg" />
+                <div className="absolute mx-auto w-full h-screen bg-white bg-opacity-30 flex flex-col justify-center items-center">
+                  <div className="h-1/2 w-full p-10 bg-gray-900 bg-opacity-60 flex flex flex-col justify-center items-center">
+                    <h1 className="text-4xl md:text-6xl font-bold text-yellow-400">ទំនាក់ទំនង</h1>
+                    <h1 className="text-4xl md:text-6xl font-bold text-yellow-400">016 777 631</h1>
+                  </div>
+                  <div className="h-1/2 w-full p-10 bg-yellow-300 bg-opacity-80 flex flex flex-col justify-center items-center">
+                    <h1 className="text-4xl md:text-6xl font-bold text-gray-900">Thank you !</h1>
+                  </div>
                 </div>
               </div>
             </div>
