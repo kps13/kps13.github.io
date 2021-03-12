9c652efe491fa84414b5f424251834b59f350aa8
samda <muy.samda13@gmail.com>
2021-03-12 15:31:44 +0700

    add pages

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,74 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import ReactFullpage from '@fullpage/react-fullpage';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect( () => {
+    AOS.init()
+  }, [])
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ReactFullpage
+      //fullpage options
+      licenseKey={'YOUR_KEY_HERE'}
+      scrollingSpeed={1000} /* Options here */
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <div style={{ background: `url(./images/brands.png) `}} className="h-full w-full">
+                <div className="w-full h-full bg-white bg-opacity-70 flex flex-col justify-end items-center">
+                  <div className="self-start mb-20 py-6 bg-yellow-400 bg-opacity-80 w-full flex justify-center items-start">
+                    <h1 className="text-6xl text-bold text-gray-900">
+                      ស្វាគមន៍មកកាន់ KPS Collection
+                    </h1>
+                  </div>
+                  <div className="w-5/6 rounded-md mb-10 px-6 py-6 bg-gray-900 bg-opacity-80 flex items-center justify-center">
+                    <h1 className="text-3xl text-bold text-yellow-300">
+                      ពួកយើងផ្តល់ជូននូវ សំលៀកបំពាក់ស្អាតៗ ទាន់សម័យ សំរាប់ បុរស នារី នឹង កុមារ
+                    </h1>
+                  </div>
+                  <div className="w-5/6 flex justify-between items-center bg-opacity-80 py-10 space-x-10">
+                    <img data-aos="fade-right" data-aos-duration="2000" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/baby.svg" />
+                    <img data-aos="fade-right" data-aos-duration="1500" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/girl.svg" />
+                    <img data-aos="fade-up" data-aos-duration="1000" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/boy.svg" />
+                    <img data-aos="fade-left" data-aos-duration="1500" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/women.svg" />
+                    <img data-aos="fade-left" data-aos-duration="2000" className="h-40 bg-yellow-300 rounded-lg bg-opacity-80 p-2" src="./images/men.svg" />
+                  </div>
+                  <div className="py-6 bg-yellow-400 bg-opacity-80 w-full flex justify-center items-start">
+                    <h1 className="text-6xl text-bold text-gray-900">
+                      នឹងមកដើលឆាប់ៗនេះ!
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="section" style={{ backgroundSize: 'cover', background: "url(./images/kids.png) no-repeat center center"}}>
+              <div className="mx-auto w-full h-screen bg-white bg-opacity-30 flex justify-center items-center">
+                <div className="p-4 rounded-lg bg-gray-900 bg-opacity-60">
+                  <h1 className="text-6xl font-bold text-yellow-400">កុមារ</h1>
+                </div>
+              </div>
+            </div>
+            <div className="section" style={{ backgroundSize: 'cover', background: "url(./images/women.png) no-repeat center center" }}>
+              <div className="mx-auto w-full h-screen bg-white bg-opacity-30 flex justify-center items-center">
+                <div className="p-4 rounded-lg bg-gray-900 bg-opacity-60">
+                  <h1 className="text-6xl font-bold text-yellow-400">នារី</h1>
+                </div>
+              </div>
+            </div>
+            <div className="section" style={{ backgroundSize: 'cover', background: "url(./images/men.jpg) no-repeat center center" }}>
+              <div className="mx-auto w-full h-screen bg-white bg-opacity-30 flex justify-center items-center">
+                <div className="p-4 rounded-lg bg-gray-900 bg-opacity-60">
+                  <h1 className="text-6xl font-bold text-yellow-400">បុរស</h1>
+                </div>
+              </div>
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   )
 }
